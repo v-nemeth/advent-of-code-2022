ba4a1db0910701b398b1bb43385685970c3869c0
v-nemeth <viand21@student.sdu.dk>
2022-12-06 15:04:17 +0100

    day 6 fun

--- solutions/day6/part_two.rb
+++ solutions/day6/part_two.rb
@@ -1,17 +1,7 @@
 Dir.chdir(File.dirname(__FILE__))
-
-input = File.read('input.txt')
-
 def matches? (string)
-  string.length.times do |i|
-    string.length.times do |j|
-      next if i == j
-      return true if string[i] == string[j]
-    end
-  end
+  string.length.times {|i| string.length.times {|j| return true if string[i] == string[j] unless i == j} }
   false
 end
-
-(input.length-14).times do |i|
-  p i+14 unless matches?(input[i..i+13])
-end
+input = File.read('input.txt')
+(input.length-14).times {|i| p i+14 unless matches?(input[i..i+13])}
